efb0efcc616a3d5d38930bee04c673aa70ec60e2
Oscar Komulainen <oscko391@student.liu.se>
2017-03-27 11:58:41 +0200

    minor changes

--- lab1/lab1/src/Puzzle.cpp
+++ lab1/lab1/src/Puzzle.cpp
@@ -1,9 +1,11 @@
 #include "Puzzle.h"
 
 
+
 Puzzle::Puzzle()
 {
     //fill board with distinct random values
+    //let zero be the empty tile
     for (int rows = 0; rows < sizeof(board)/sizeof(*board); rows++){
         for (int cols = 0; cols < sizeof(*board)/sizeof(*board[0]); cols++){
             while (1){
@@ -13,6 +15,7 @@ Puzzle::Puzzle()
                 //check for the value in the board
                 for (int i = 0; i < rows; i++){
                     for (int j = 0; j < cols; j++){
+                        int temp = board[i][j];
                         if (value == board[i][j]) {
                             alreadyPresent = true;
                             break;
@@ -34,6 +37,7 @@ Puzzle::~Puzzle()
     //dtor
 }
 
+//prints values in the board
 void Puzzle::print()
 {
     for (int rows = 0; rows < sizeof(board)/sizeof(*board); rows++){
