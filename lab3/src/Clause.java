da88a8800efff3e1c101d713a62bdac6659062cd
Adam Söderström <adaso578@student.liu.se>
2017-05-05 12:01:17 +0200

    Lab3

--- lab3/src/Clause.java
+++ lab3/src/Clause.java
@@ -2,8 +2,12 @@ import java.util.Vector;
 
 public class Clause {
 	
-	private Vector<String> negVec = new Vector<String>();
-	private Vector<String> posVec = new Vector<String>();
+	public Vector<String> negVec = new Vector<String>();
+	public Vector<String> posVec = new Vector<String>();
+	
+	Clause(){
+		;
+	}
 	
 	Clause(String theString){
 		// Clear whitespaces
@@ -24,46 +28,76 @@ public class Clause {
 				posVec.addElement(parts[i]);
 			}
 		}
-
-		this.simplify();
 	}
 
     /**
      * Simplifies the clause by removing elements in both negVec and posVec
      */
     public void simplify(){
-		for(int i = 0; i < negVec.size(); i++){
-			for(int j = 0; j < posVec.size(); j++){
-				if(negVec.get(i).equals(posVec.get(j))){
+    	
+    	int i=0;
+    	int j=0;
+    	
+    	// Compare negVec with posVec
+    	while(i<negVec.size()){
+    		while(j<posVec.size()){
+    			
+    			if(negVec.get(i).equals(posVec.get(j))){
 					//Remove from both negVec and posVec
                     negVec.removeElementAt(i);
                     posVec.removeElementAt(j);
+                    
 				}
-
-			}
-		}
+    			j++;
+    		}
+    		i++;
+    	}
+    	
+    	i=0;
+    	j=0;
+    	
+    	// Compare posVec with negVec
+    	while(i<posVec.size()){
+    		while(j<negVec.size()){
+    			
+    			if(posVec.get(i).equals(negVec.get(j))){
+					//Remove from both negVec and posVec
+                    posVec.removeElementAt(i);
+                    negVec.removeElementAt(j);
+                    
+				}
+    			j++;
+    		}
+    		i++;
+    	}	
+		
 	}
 	
 	
 	public void display(){
 
-		System.out.println("Clause: ");
+		System.out.print("[");
 
 		if(negVec.size() > 0){
-			System.out.print("   negVec: ");
 			for(int i=0; i<negVec.size(); i++){
-				System.out.print(negVec.elementAt(i)+" ");
+				
+				System.out.print("-"+negVec.elementAt(i));
+				if(i != negVec.size()-1)
+					System.out.print(" V ");
 			}
-			System.out.print("\n");
 		}
 		
+		if(posVec.size() > 0 && negVec.size() > 0)
+			System.out.print(" V ");
+		
 		if(posVec.size() > 0){
-			System.out.print("   posVec: ");
 			for(int i=0; i<posVec.size(); i++){
-				System.out.print(posVec.elementAt(i)+" ");
+				System.out.print(posVec.elementAt(i));
+				if(i != posVec.size()-1)
+					System.out.print(" V ");
 			}
-			System.out.println();
 		}
+		System.out.print("] \n");
 	}
 
 
--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[] formulae = {"-a V a", "a", "-b"};
+        String[] formulae = {"-a V b", "-b", "-a", "a"};
         
         Engine theEngine = new Engine(formulae);
         theEngine.resolve();
